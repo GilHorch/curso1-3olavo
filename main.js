da9e848adb576803eb9e5a5cb325dace4735ed6e
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-03-13 11:56:17 -0300

    13

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ for (let i = 0; i < botoes.length; i++) {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
         }
-
         botoes[i].classList.add("ativo");
     }
     console.log();
