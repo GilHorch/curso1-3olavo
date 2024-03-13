9985c80d32d9933a5392438b11cd1bcb4c76cfc8
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-03-13 11:50:01 -0300

    13

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
-            botoes[i].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
