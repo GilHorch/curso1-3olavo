b8d0135ea3b659f8d1353a33fd240f3a3488c14a
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-03-13 11:48:25 -0300

    13

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[i].classList.remove("ativo");
+        }
+
         botoes[i].classList.add("ativo");
     }
     console.log();
