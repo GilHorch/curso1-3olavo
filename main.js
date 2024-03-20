01dab8ca61faf0f582440242ca12dc70fbb0a34c
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-03-20 11:38:55 -0300

    20

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");//variável que armazena todos os itens com a classe botão
 
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantidade de botoes
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
+    botoes[i].onclick = function () {//cria uma função 
+
+        for (let j = 0; j < botoes.length; j++) { // loop que executa enquanto j<quantidade de botoes
+            botoes[j].classList.remove("ativo");  //remove a palavra ativo da classe.
         }
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
     console.log();
 }
