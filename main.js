f765ee2d6e507c883382c21213ad34121be4219a
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-03-27 11:52:03 -0300

    27

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantida
 
         for (let j = 0; j < botoes.length; j++) { // loop que executa enquanto j<quantidade de botoes
             botoes[j].classList.remove("ativo");  //remove a palavra ativo da classe.
+            textos[j].classList.remove("ativo");  //remove a palavra ativo da classe.
         }
         botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
         textos[i].classList.add("ativo");//adiciona a palavra ativo da classe.
