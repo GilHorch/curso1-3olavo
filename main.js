32632bb45539945cfd5f9cef692bb40a8e9bc0c3
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 11:17:57 -0300

    1

--- main.js
+++ main.js
@@ -30,5 +30,5 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     return [dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos"];
-    print(dias);
+    //print(dias);
 }
