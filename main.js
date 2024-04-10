0406465aacb6bc4d019e49844372257c53e75be9
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 10:51:10 -0300

    1

--- main.js
+++ main.js
@@ -28,5 +28,9 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    return dias+"dias"+ horas+"horas"+minutos+"minutos"+segundos+"segundos";
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+    
+    return [dias+"dias"+ horas+"horas"+minutos+"minutos"+segundos+"segundos"];
 }
