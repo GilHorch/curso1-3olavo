1904e113bcfc9fc0b837cf1d2a2100b95046f0a9
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 11:14:06 -0300

    1

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantida
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
