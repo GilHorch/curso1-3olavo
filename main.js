455dfb17b3bf100339ede673a1c030bafa9a211e
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 11:05:05 -0300

    1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
