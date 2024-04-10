567a2cd100ce867e47e577577817223361f80164
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 11:42:30 -0300

    1

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo1);
-contadores[2].textContent = calculaTempo(tempoObjetivo1);
-contadores[3].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo3);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
