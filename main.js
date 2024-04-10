af23b6a62fbef3e23391216fb5cbe5afe733ba23
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 10:56:41 -0300

    1

--- main.js
+++ main.js
@@ -21,16 +21,16 @@ const tempoObjetivo1 = new Date("24-10-06T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
-
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    segundos%=60;
-    minutos%=60;
-    horas%=24;
-    
-    return [dias+"dias"+ horas+"horas"+minutos+"minutos"+segundos+"segundos"];
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
