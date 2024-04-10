3d566c00803c626d585a2f8cac806c4afd036c74
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-10 12:00:06 -0300

    1

--- main.js
+++ main.js
@@ -20,10 +20,9 @@ const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 
-const tempos=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-for (let i=0;i<contadores.length;i++)
-{
+for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i]);
 }
 
