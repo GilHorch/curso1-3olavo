d1872b259a4a64849bd7a1479a0f43ea1175b473
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-04-17 11:20:53 -0300

    '

--- main.js
+++ main.js
@@ -34,11 +34,12 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+
     if (tempoFinal > 0) {
         return [dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos"];
         //print(dias);
     }
-    else{return "Pazo Finalizado"}
+    else{return "Prazo Finalizado"}
 }
 function atualizaCronometro() {
     for (let i = o; i < contadores.length; i++) {
